6b00e88a1c0ef9f0c08132e4d890390790bc8e69
Allan <ag.pandochi@gmail.com>
2026-06-23 13:11:52 -0300

    começando o conteudo

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
-import { Heading } from './styles/components/Heading';
+
 
 import './styles/theme.css';
 import './styles/global.css';
 
+
 export function App() {
-    console.log('Oi');
 
     return (
-        <> 
-        {/* podemos utilizar as tags vazias ao inves de usar divs, pois o react só permite um elemento pai */}
-           <Heading>Olá mundo!</Heading> 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati mollitia odit voluptate. Culpa optio voluptatem assumenda nisi voluptate, molestiae magni, ad inventore autem, illum sequi neque deleniti ducimus! Praesentium, sunt?</p>
-        </>
+    <>
+        <div className="container">
+            <div className="content">
+                <section>LOGO</section>
+            </div>
+        </div>
+        <div className="container">
+            <div className="content">
+                <section>MENU</section>
+            </div>
+        </div>
+        <div className="container">
+            <div className="content">
+                <section>FORM</section>
+            </div>
+        </div>
+        <div className="container">
+            <div className="content">
+                <section>FOOTER</section>
+            </div>
+        </div>
+    </>
     );
 }
 
-// export default App; dessa forma eu posso nomear a minha function da maneira que eu desejar
- //dessa forma eu só posso importar usando {App}
--- src/styles/components/Heading.tsx
+++ src/styles/components/Heading.tsx
@@ -1,6 +1,13 @@
-import styles from './Heading.module.css'
+import type React from 'react';
+import styles from './Heading.module.css';
 
-export function Heading(props) {
-    // return <h1 className={styles.heading}>Olá mundo!</h1>
-    return <h1 className={`${styles.heading} ${styles.cyan}`}>{props.children}</h1>
+type HeadingProps = {
+    children: React.ReactNode; //tudo que o react aceita como children está nessa tipagem
+}; //no typescript fazemos assim para fazer a tipagem do props
+
+export function Heading({ children }: HeadingProps)//posso fazer dessa forma tambem
+ {
+    return <h1 className={styles.heading}>{ children }</h1>
+    //const { children } = props //dessa forma eu não preciso usar o objeto que seria dessa forma -> const children = props.children;
+    //return <h1 className={`${styles.heading} ${styles.cyan}`}>{children}</h1>
 }
