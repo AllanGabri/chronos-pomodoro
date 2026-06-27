7446e111a35c10931a655c077b2e3aa41fd29ff4
Allan <ag.pandochi@gmail.com>
2026-06-26 21:01:28 -0300

    initializing some functions

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
 
+import { Footer } from './components/Footer';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -40,14 +44,16 @@ export function App() {
                     </div>
 
                     <div className="formRow">
-                        <p>Ciclos</p>
-                        <p>0 0 0 0 0 0 0</p>
+                        <Cycles/>
                     </div>
 
                     <div className="formRow">
-                        <button>Enviar</button>
+                       <DefaultButton icon={<PlayCircleIcon/>} color= 'green'/>
                     </div>
                 </form>
+            </Container>
+            <Container>
+                <Footer/>
             </Container>
 
         </>
